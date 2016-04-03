8fa01c8c3da70cc4e6917be4b9a1ee7e07197f4c
mateus <mtsbarbosa@hotmail.com>
2016-04-03 00:15:03 -0300

    DEV_7: solving issues with heroku deploy

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -50,6 +50,8 @@ Devise.setup do |config|
   # modifying a user and when used to authenticate or find a user. Default is :email.
   config.strip_whitespace_keys = [:email]
 
+  config.secret_key = ENV['DEVISE_SECRET_KEY'] if Rails.env.production?
+
   # Tell if authentication through request.params is enabled. True by default.
   # It can be set to an array that will enable params authentication only for the
   # given strategies, for example, `config.params_authenticatable = [:database]` will
